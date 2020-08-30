e62b1e97fe25be96a5f7aabe94aa29d5c13b8e23
KAIYOHUGO <KAIYOHUGO@users.noreply.github.com>
2020-08-30 20:01:05 +0800

    pwa 6

--- ig/js/main.js
+++ ig/js/main.js
@@ -4,8 +4,6 @@ $(function() {
     offset: 200
   });
   // CODELAB: Register service worker.
-  import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
-
   const el = document.createElement('pwa-update');
   document.body.appendChild(el);
 
